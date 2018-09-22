9e441fc91640ed9e71ed0732937946ec4f25f6a0
Nabil Ridhwanshah <35335428+builtbynabil@users.noreply.github.com>
2018-09-22 21:51:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+$("body").css("height", window.outerHeight+100 + "px")
+
+var app = new Vue({
+
+    el: ".root",
+    data: {
+        "color1": "",
+        "color2": "",
+    },
+
+    methods:{
+        updatecolor(){
+            
+            if(this.color2){
+                console.log("color 2 is here");
+                $("body").css("background-image", "linear-gradient(" + this.color1 + "," + this.color2 + ")")
+            }else{
+                console.log("color 2 is not filled");
+                $("body").css("background-color", app.color1)
+
+            }
+
+        },
+
+        randomcolor1(){
+            this.color1 = "#" + Math.random().toString(16).slice(2, 8);
+        },
+
+        randomcolor2(){
+            this.color2 = "#" + Math.random().toString(16).slice(2, 8);
+        },
+
+        closeinstructions(){
+            $("#instructions").fadeOut(600);
+        },
+
+        hideformdiv(){
+            $(".colorform").toggle(600);
+
+        }
+    }
+    
+})
+
