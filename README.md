3a25bf181da377e0bbdf839bf6840876b5d364f8
Nabil Ridhwanshah <35335428+builtbynabil@users.noreply.github.com>
2018-09-29 08:59:53 +0800

    updated site [read me for more info]

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # [colorgen](https://builtbynabil.github.io/colorgen)
 ##### by builtbynabil
 
-color generator app created using Vue
+gradient background generator app created using Vue
 
 ## Instructions
-- Generate a color using HEX or just use the randomize color button
-- Press the 'Apply Background' button
+- Input both colors and then press the apply or press the randomize button which will create a random background
 - Press the top of the page to hide the form
 - Screenshot and crop and make it as your new wallpaper
 
 ## Todos
 - [x] Improve the site in terms of UX and UI
 - [ ] Make users able to save images [Right click or hold to save image]
-- [ ] Better interface to hide the color form
+- [x] Better interface to hide the color form [Nothing to do about it]
 - [ ] Integrate color pickers
--- main.js
+++ main.js
@@ -26,22 +26,42 @@ var app = new Vue({
       color =
         "#" +
         Math.random()
-          .toString(16)
-          .slice(2, 8);
+        .toString(16)
+        .slice(2, 8);
       this.input = color;
     },
     randomizeInput2() {
       color =
         "#" +
         Math.random()
-          .toString(16)
-          .slice(2, 8);
+        .toString(16)
+        .slice(2, 8);
       this.input2 = color;
     },
 
     toggleform() {
-        $(".block").slideToggle(500)
+      $(".block").slideToggle(500)
+    },
+    randomizeBackground() {
+      color1 = "#" + Math.random().toString(16).slice(2, 8);
+      color2 = "#" + Math.random().toString(16).slice(2, 8);
+      $("body").css("background-image", "linear-gradient(" + color1 + "," + color2 + ")");
+    },
+    reverseinputs(){
+      firstinput = this.input
+      this.input = this.input2
+      this.input2 = firstinput
+
+      if (this.input2 == "") {
+        $("body").css("background", this.input);
+      } else {
+        $("body").css(
+          "background-image",
+          "linear-gradient(" + this.input + "," + this.input2 + ")"
+        );
+      }
+      
     }
-    
+
   }
 });
